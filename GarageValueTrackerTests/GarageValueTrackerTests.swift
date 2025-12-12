ed59fd26f375bba97d06e99857aac55b50f64fda
raama srivatsan <raama@me.com>
2025-12-11 19:19:52 -0800

    Initial Commit

---
+++ GarageValueTrackerTests/GarageValueTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GarageValueTrackerTests.swift
+//  GarageValueTrackerTests
+//
+//  Created by raama srivatsan on 12/11/25.
+//
+
+import Testing
+@testable import GarageValueTracker
+
+struct GarageValueTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
